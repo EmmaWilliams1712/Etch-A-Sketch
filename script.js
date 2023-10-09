4d5c2e3e17b3a8629b9dc7729ac41dde2258840c
Emma Williams <143957702+EmmaWilliams1712@users.noreply.github.com>
2023-10-09 16:55:57 +0200

    Modified javascript to allow the grid to be reset to any size up to 100 x 100, added the hover effect to change background colour and added button to html for the reset option. Added styling also with CSS

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const gridContainer = document.getElementById("grid-container");
+const container = document.getElementById("grid-container");
+const resetButton = document.getElementById("reset-button");
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+function createGrid(size) {
+    container.innerHTML = "";
+    container.style.setProperty("--grid-size", size);
+
+
+    for (let i = 0; i < size * size; i++) {
         const gridItem = document.createElement("div");
-        gridItem.classList.add("grid-item");
+        gridItem.classList.add("gridItem");
+      
 
-        gridContainer.appendChild(gridItem);
+        gridItem.addEventListener("mouseenter", () => {
+            gridItem.style.backgroundColor = "black";
+        });
+
+        container.appendChild(gridItem);
     }
 }
+
+function resetGrid() {
+    const newSize = prompt("Enter the number of squares per side (max 100):");
+    if (newSize && newSize > 0 && newSize <= 100) {
+        createGrid(newSize);
+    } else {
+        alert("Please enter a valid number between 1 and 100.");
+    }
+}
+
+resetButton.addEventListener("click", resetGrid);
+
+createGrid(16);
